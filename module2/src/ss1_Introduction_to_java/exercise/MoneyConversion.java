ac38bce243f375cde26b273e09173bac108e858e
sontran987 <sonva2002@gmail.com>
2023-03-06 16:57:18 +0700

    fist commit

--- module2/src/ss1_Introduction_to_java/exercise/MoneyConversion.java
+++ module2/src/ss1_Introduction_to_java/exercise/MoneyConversion.java
@@ -4,10 +4,10 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MoneyConversion {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Nhập số tiền: USD");
-        Scanner scanner=new Scanner(System.in);
-        int money=Integer.parseInt(scanner.nextLine())*23000;
-        System.out.println("Số tiền Việt Nam là: "+ money);
+        Scanner scanner = new Scanner(System.in);
+        int money = Integer.parseInt(scanner.nextLine()) * 23000;
+        System.out.println("Số tiền Việt Nam là: " + money);
     }
 }
