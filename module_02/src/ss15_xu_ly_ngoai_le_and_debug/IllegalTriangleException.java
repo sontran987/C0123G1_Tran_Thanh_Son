a867214adbc3f1860a4f795921998fd902d5408a
sontran987 <sonva2002@gmail.com>
2023-03-23 15:45:09 +0700

    thanhson_updete

--- module_02/src/ss15_xu_ly_ngoai_le_and_debug/IllegalTriangleException.java
+++ module_02/src/ss15_xu_ly_ngoai_le_and_debug/IllegalTriangleException.java
@@ -1,6 +1,6 @@
 package ss15_xu_ly_ngoai_le_and_debug;
 
-public class IllegalTriangleException extends Exception{
+public class IllegalTriangleException extends Exception {
     public IllegalTriangleException(String message) {
         super(message);
     }
