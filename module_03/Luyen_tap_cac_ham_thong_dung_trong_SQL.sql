1505df0ab83079b1c1901ade7b5fb49d66772082
Tran Thanh Son <sonva2002@gmail.com>
2023-04-14 17:19:51 +0700

    update

---
+++ module_03/Luyen_tap_cac_ham_thong_dung_trong_SQL.sql
@@ -0,0 +1,48 @@
+USE quan_ly_sinh_vien;
+-- part 1
+SELECT address, COUNT(student_id) AS so_luong_hoc_vien
+FROM student
+GROUP BY address;
+-- part 2
+SELECT 
+    s.student_id, s.student_name, AVG(mark.mark)
+FROM
+    student s
+        JOIN
+    mark ON mark.student_id = s.student_id
+GROUP BY s.student_id , s.student_name;
+
+-- exercise
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+SELECT 
+     s.sub_id,s.sub_name,s.statuss, MAX(credit) AS credit_max
+FROM
+    subjects s
+GROUP BY 
+s.sub_id,
+s.sub_name,
+s.statuss
+ORDER BY credit_max DESC
+LIMIT 1;
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+SELECT s.sub_id,s.sub_name,s.statuss,s.statuss ,MAX(mark) AS mark
+FROM mark m
+INNER JOIN subjects s ON s.sub_id=m.sub_id
+GROUP BY 
+s.sub_id,s.sub_name,s.statuss,s.statuss
+ORDER BY 
+mark DESC
+LIMIT 1;
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+
+SELECT s.student_id ,s.student_name , s.address ,s.phone ,s.statuss,s.classes_id,ROUND(AVG(m.mark),0) AS diem_trung_binh
+FROM student s
+INNER JOIN mark m ON m.student_id=s.student_id
+GROUP BY
+s.student_id
+ORDER BY
+diem_trung_binh desc;
+
+
+
+
--- module_03/furama.sql
+++ module_03/furama.sql
@@ -214,21 +214,129 @@ WHERE
         OR dia_chi LIKE '%Quảng Trị');
 
 -- -- -- -- -- -- task 4-- -- -- -- -- 
-SET SQL_SAFE_UPDATES=0;
 SELECT 
-	kh.ma_khach_hang,kh.ho_ten,COUNT(ho_ten) AS "so_lan_dat_phong"
+    kh.ma_khach_hang,
+    kh.ho_ten,
+    COUNT(kh.ma_khach_hang) AS so_lan_dat_phong
 FROM
     khach_hang kh
-GROUP BY
-    kh.ho_ten
-ORDER BY
-    so_luong_dat_phong ASC;
+        INNER JOIN
+    hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+        INNER JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+WHERE
+    lk.ten_loai_khach = 'Diamond'
+GROUP BY kh.ma_khach_hang
+ORDER BY so_lan_dat_phong;
     
-    -- : Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet
     -- --- -- -- task 5-- -- --
 SELECT 
-    kh.ma_khach_hang,kh.ho_ten,lh.ten_loai_khach,hd.ma_hop_dong,dich_vu.ten_dich_vu,
-    hd.ngay_lam_hop_dong,hd.ngay_ket_thuc,SUM(hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia) AS 'tong_tien'
-FROM khach_hang kh
-INNER JOIN loai_khach lh ON lh.ma_loai_khach = kh.ma_loai_khach
-INNER JOIN hop_dong hd ON hd.ma_dich_vu = dich_vu.ma_dich_vu
+    kh.ma_khach_hang,
+    kh.ho_ten ,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dich_vu.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    (dich_vu.chi_phi_thue+ifnull((hdct.so_luong * dvdk.gia),0)) AS tong_tien
+FROM
+    khach_hang kh
+        INNER JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+    LEFT JOIN 
+    hop_dong hd ON hd.ma_khach_hang=kh.ma_khach_hang
+    LEFT JOIN
+    dich_vu ON hd.ma_dich_vu = dich_vu.ma_dich_vu
+    LEFT JOIN 
+    hop_dong_chi_tiet hdct ON hd.ma_hop_dong=hdct.ma_hop_dong
+    LEFT JOIN 
+    dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem=dvdk.ma_dich_vu_di_kem
+    ORDER BY 
+    kh.ma_khach_hang;
+    
+    -- --- -- ------task 6--------
+    -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu
+    -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của 
+    -- năm 2021 (Quý 1 là tháng 1, 2, 3).
+   
+    SELECT 
+    dv.ma_dich_vu,
+    dv.ten_dich_vu,
+    dv.dien_tich,
+    dv.chi_phi_thue,
+    ldv.ten_loai_dich_vu
+FROM
+    dich_vu dv
+        INNER JOIN
+    loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+WHERE
+    ldv.ten_loai_dich_vu NOT IN (SELECT 
+            ldv.ten_loai_dich_vu
+        FROM
+            loai_dich_vu ldv
+                INNER JOIN
+            dich_vu dv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+                INNER JOIN
+            hop_dong hd ON hd.ma_dich_vu = dv.ma_dich_vu
+        WHERE
+            (MONTH(hd.ngay_lam_hop_dong)) <= 3
+                AND (YEAR(hd.ngay_lam_hop_dong)) = 2021)                
+ORDER BY 
+ldv.ten_loai_dich_vu DESC
+;
+    
+   -- --------task 7 ----------
+    -- 7.Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, 
+    -- chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách 
+    -- hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+    
+    SELECT 
+    dv.ma_dich_vu,
+    dv.ten_dich_vu,
+    dv.dien_tich,
+    dv.so_nguoi_toi_da,
+    dv.chi_phi_thue,
+    ldv.ten_loai_dich_vu
+FROM
+    dich_vu dv
+        INNER JOIN
+    loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+        INNER JOIN
+    hop_dong hd ON hd.ma_dich_vu = dv.ma_dich_vu
+WHERE
+    (YEAR(hd.ngay_lam_hop_dong)) = 2020
+        AND hd.ma_dich_vu NOT IN (SELECT 
+            hd.ma_dich_vu
+        FROM
+            loai_dich_vu ldv
+                INNER JOIN
+            dich_vu dv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+                INNER JOIN
+            hop_dong hd ON hd.ma_dich_vu = dv.ma_dich_vu
+        WHERE
+            (YEAR(hd.ngay_lam_hop_dong)) = 2021)
+GROUP BY dv.ma_dich_vu
+ORDER BY dv.ma_dich_vu;
+-- -- ----- task 8 ---------
+    -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+	-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+-- way 1: 
+SELECT kh.ho_ten 
+FROM khach_hang kh 
+GROUP BY 
+kh.ho_ten;
+-- way 2: 
+SELECT DISTINCT kh.ho_ten 
+FROM khach_hang kh;
+  -- way 3: 
+
+-- --------task 9-- --------
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng
+-- trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+SELECT MONTH(hd.ngay_lam_hop_dong) AS thang, COUNT(hd.ma_khach_hang) AS so_luong_doanh_thu
+FROM hop_dong hd
+GROUP BY thang
+ORDER BY 
+thang 
+  
+  
