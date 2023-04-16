8c30652afc1486c9b153665330ae0f1489512fe6
Tran Thanh Son <sonva2002@gmail.com>
2023-04-16 17:00:36 +0700

    update

--- module_03/furama.sql
+++ module_03/furama.sql
@@ -349,17 +349,35 @@ ORDER BY thang;
   -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
   
   
-  SELECT hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, IFNULL(SUM(hdct.so_luong),0) AS so_luong_dich_vu_di_kem
-  FROM hop_dong hd
-  LEFT JOIN hop_dong_chi_tiet hdct ON hdct.ma_hop_dong=hd.ma_hop_dong
-  GROUP BY 
-  hd.ma_hop_dong;
+  SELECT 
+    hd.ma_hop_dong,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    hd.tien_dat_coc,
+    IFNULL(SUM(hdct.so_luong), 0) AS so_luong_dich_vu_di_kem
+FROM
+    hop_dong hd
+        LEFT JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+GROUP BY hd.ma_hop_dong;
   
   -- ------ task 11 ----------
   -- 11.Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng 
   -- bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
-  
-  
+  SELECT 
+    dvdk.*
+FROM
+    dich_vu_di_kem dvdk
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+        INNER JOIN
+    hop_dong hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+        INNER JOIN
+    khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+        INNER JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+WHERE
+    lk.ten_loai_khach LIKE '%Diamond';
   
   -- --------task 12 -----------
   -- 12.Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng),
@@ -367,3 +385,94 @@ ORDER BY thang;
   -- việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được 
   -- khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
   
+  SELECT 
+    hd.ma_hop_dong,
+    nv.ho_ten,
+    kh.ho_ten,
+    kh.so_dien_thoai,
+    dv.ten_dich_vu,
+    SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem,
+    hd.tien_dat_coc
+FROM
+    hop_dong hd
+        INNER JOIN
+    nhan_vien nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+        INNER JOIN
+    khach_hang kh ON kh.ma_khach_hang = hd.ma_hop_dong
+        INNER JOIN
+    dich_vu dv ON dv.ma_dich_vu = dv.ma_dich_vu
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+        INNER JOIN
+    dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+    WHERE  
+    MONTH(YEAR(hd.ngay_lam_hop_dong)=2020)>9 AND NOT MONTH(YEAR(hd.ngay_lam_hop_dong)=2021)<7
+  GROUP BY 
+  hd.ma_hop_dong,
+    nv.ho_ten,
+    kh.ho_ten,
+    kh.so_dien_thoai,
+    dv.ten_dich_vu,
+    hd.tien_dat_coc;
+  
+  -- -------- task 13 -----------
+  -- 13.Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+  -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+  SELECT 
+    dvdk.ma_dich_vu_di_kem,
+    dvdk.ten_dich_vu_di_kem,
+    SUM(hdct.so_luong) AS so_luong_dich_di_kem
+FROM
+    dich_vu_di_kem dvdk
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY dvdk.ma_dich_vu_di_kem,
+ dvdk.ten_dich_vu_di_kem
+ ORDER BY 
+ dvdk.ma_dich_vu_di_kem;
+  -- -------- task 14 ----------
+  -- 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+  -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
+  -- (được tính dựa trên việc count các ma_dich_vu_di_kem).
+  
+SELECT 
+    hd.ma_hop_dong,
+    ldv.ten_loai_dich_vu,
+    dvdk.ten_dich_vu_di_kem,
+    COUNT(hdct.ma_dich_vu_di_kem) = 1 AS so_lan_su_dung
+FROM
+    hop_dong hd
+        INNER JOIN
+    dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        INNER JOIN
+    loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+        INNER JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+        INNER JOIN
+    dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+GROUP BY hd.ma_hop_dong , ldv.ten_loai_dich_vu , dvdk.ten_dich_vu_di_kem
+ORDER BY hd.ma_hop_dong;
+-- ------------task 15 -------------
+-- 15.Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, 
+-- ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 
+-- 3 hợp đồng từ năm 2020 đến 2021.
+
+SELECT MAX(nv.ma_nhan_vien) AS ma_nhan_vien,
+ nv.ho_ten,
+ td.ten_trinh_do,
+ bp.ten_bo_phan,
+ nv.so_dien_thoai,
+ nv.dia_chi
+ FROM nhan_vien nv
+ INNER JOIN trinh_do td ON td.ma_trinh_do=nv.ma_nhan_vien
+ INNER JOIN bo_phan bp ON bp.ma_bo_phan=nv.ma_bo_phan
+ INNER JOIN hop_dong hd ON hd.ma_nhan_vien=nv.ma_nhan_vien
+WHERE (YEAR(hd.ngay_lam_hop_dong)BETWEEN 2020 AND 2021)
+GROUP BY
+ nv.ho_ten,
+ td.ten_trinh_do,
+ bp.ten_bo_phan,
+ nv.so_dien_thoai,
+ nv.dia_chi;
+
+
