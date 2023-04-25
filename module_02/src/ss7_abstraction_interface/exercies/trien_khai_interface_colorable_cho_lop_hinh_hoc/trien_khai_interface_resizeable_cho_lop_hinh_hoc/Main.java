497ab53da3c7ebf46d1f4b8f21d196db4f3e2f98
Tran Thanh Son <sonva2002@gmail.com>
2023-04-25 15:54:44 +0700

    up

--- module_02/src/ss7_abstraction_interface/exercies/trien_khai_interface_colorable_cho_lop_hinh_hoc/trien_khai_interface_resizeable_cho_lop_hinh_hoc/Main.java
+++ module_02/src/ss7_abstraction_interface/exercies/trien_khai_interface_colorable_cho_lop_hinh_hoc/trien_khai_interface_resizeable_cho_lop_hinh_hoc/Main.java
@@ -4,15 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[3];
-        shapes[0] = new Circle();
-        shapes[1] = new Square();
-        shapes[2] = new Rectangle();
-        System.out.println(Arrays.toString(shapes));
-        for (Shape s : shapes) {
-            if (s instanceof Colorable) {
-                ((Colorable) s).howToColor();
-            }
-        }
+
     }
 }
