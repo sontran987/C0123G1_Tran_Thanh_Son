48b1e8205dd56ddc21e9d740c7f002fe12e235a0
Tran Thanh Son <sonva2002@gmail.com>
2023-06-05 14:17:06 +0700

    up

--- module_3_lan_hai/ss2.sql
+++ module_3_lan_hai/ss2.sql
@@ -8,14 +8,24 @@ CREATE TABLE students (
     class_name VARCHAR(50) NULL
 );
 CREATE TABLE authors(
-id INT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 name_authors VARCHAR(50) NOT NULL
 );
+ INSERT into authors(name_authors) VALUES
+ ('thanh Son'),
+ ('thanhkim'),
+ ('quoc thiet'),
+ ('quang dang');
 
 CREATE TABLE cate_gory (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name_cate_gory VARCHAR(50) NOT NULL
 );
+INSERT into cate_gory(name_cate_gory) VALUES
+('tieu thuyet'),
+('truyen tranh'),
+('sach'),
+('phi kim');
 CREATE TABLE books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
@@ -35,4 +45,22 @@ borrow_date VARCHAR(50) NOT NULL,
 return_date VARCHAR(50) NOT NULL
 
 );
-UPDATE 
+SELECT * from books where books.title LIKE '%a%';
+DELETE from books where id = ? ;
+
+DELIMITER //
+CREATE PROCEDURE sp_add_book(IN title VARCHAR(50) ,IN page_size INT , IN author_id INT, IN category_id INT)
+BEGIN
+INSERT INTO books (title,page_size,author_id,cate_gory_id) VALUES
+(title,page_size,author_id,category_id);
+END //
+DELIMITER ;
+call sp_add_book(?,?,?,?);
+DELIMITER //
+CREATE PROCEDURE sp_delete_books(IN id INT)
+BEGIN
+DELETE from books where books.id=id;
+END //
+DELIMITER ;
+
+
